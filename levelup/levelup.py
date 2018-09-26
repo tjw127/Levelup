6f8abb108e7bd0787d95eae8180f63f2425f5b28
Ty Wade <tywade09@gmail.com>
2018-09-26 19:39:01 -0400

    New Changes

--- levelup/levelup.py
+++ levelup/levelup.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class LevelupEnum(Enum):
-
+    api_key = 'JG7qDgHf9nx3uhTsyTiKvekc4ciw4XAoVDKCzddRKiM6AVYX4nz7WEpcvjm6Vogs'
+    client_secret = 'Cf4z1xDQuSPRugdoXgrDmhq5JekEgMK8HQg9LwwTG2wxxt5pKPx4ZC5aj95eh8AF'
     base_url = 'https://sandbox.thelevelup.com'
     version = 'v15'
-
